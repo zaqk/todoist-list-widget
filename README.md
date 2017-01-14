467e864af84dbef044453a6e9f93d0fc0b01796b
zaqk <zachking1218@gmail.com>
2017-01-14 14:16:25 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple Dashing widget to display your todoist to-do's. 
+
+This widget has been forked from a similar repo and has thus inherited a cool color changing feature based on your todoist karma.
+
+Check out https://shopify.github.com/dashing for more information on the Dashing framework.
+Check out https://github.com/squeekobenelli/Todoist-Dashing-Widget for more information on the original todoist karma widget.
---
+++ jobs/todoist.rb
@@ -0,0 +1,32 @@
+require "net/http"
+require "json"
+
+todoist_token = ''
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+    
+    item_url_string  = 'https://todoist.com/API/v7/sync?token=' + todoist_token + '&resource_types=["items"]&sync_token=\'*\''
+    encoded_item_url_string = URI.encode(item_url_string)
+
+    item_uri = URI.parse(encoded_item_url_string)
+    http = Net::HTTP.new(item_uri.host, item_uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(item_uri.request_uri)
+    response = http.request(request)
+
+    if response.code == "200"
+        result = JSON.parse(response.body)
+        items = result['items']
+        items_array = Array[]
+        items.each do |st|
+             items_array.push(st['content'])
+        end
+        puts items_array
+        send_event('todoist', {items: items_array})
+    else
+        puts response.code
+        puts "NOT 200"
+        puts response.body
+        puts item_url_string
+    end
+end
